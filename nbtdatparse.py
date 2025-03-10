97bd525fb72ecf7326f933807cf2bd5325948111
juke32 <juke32@reborn.com>
2025-03-10 16:17:45 -0400

    Update nbtdatparse.py

--- nbtdatparse.py
+++ nbtdatparse.py
@@ -2,11 +2,11 @@ import nbtlib
 import os
 from openpyxl import Workbook
 
-# Set the directory path
-# Save the workbook to location, I think os.getcwd() would do the directory where the .py is saved
+# Set the directory path, I have examples, only have one uncommented
 
-#directory_path = 'D:\dump' #specific directory
-directory_path = os.getcwd() #current directory
+#directory_path = 'C:/Users/juke32/AppData/Roaming/.minecraft/saves' #specific directory
+directory_path = 'D:/dump'          #specific directory
+#directory_path = os.getcwd()       # broken may use root directory not current
 
 
 
@@ -141,8 +141,9 @@ for root, dirs, files in os.walk(directory_path):
 
 # Save the workbook to location, I think os.getcwd() would do the directory where the .py is saved
 
-#output_dir = "D:/dump" #specific directory
-output_dir = os.getcwd() #current directory
+#output_dir = "C:/Users/juke32/AppData/Roaming/.minecraft/saves" #specific directory
+output_dir = "D:/dump"          #specific directory
+#output_dir = os.getcwd()       # broken may use root directory not current
 
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
